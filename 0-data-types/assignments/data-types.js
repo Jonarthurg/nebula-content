8de8dc5787ef98bdf52e60a4868fa3e00721135c
jonarthurg <jonarthurg@gmail.com>
2022-05-04 16:37:13 -0400

    added test assignment

--- 0-data-types/assignments/data-types.js
+++ 0-data-types/assignments/data-types.js
@@ -1,1 +1,32 @@
 //data-types homework
+
+function plusUltra (num){
+
+    return num += 1;
+
+}
+
+console.log(plusUltra(1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function oddNums (num)
+
+for( i = 1; i <= num; i+=2){
+    console.log(i)
+}
+
+oddNums(85)
+
+//hey
