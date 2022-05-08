2172a190dbaa910f32dcbf03554747509fa3b052
jonarthurg <jonarthurg@gmail.com>
2022-05-08 15:26:54 -0400

    added variable homework.

---
+++ 1-variables/assignments/homework.js
@@ -0,0 +1,19 @@
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+const type = "Dog"
+const color = "Black"
+const age = 5
+const isTrained = true
+const legs = 4
+const friendly = false
+
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+const make = "Dodge"
+const color1 = "red"
+const doors = 4
+const hybrid = false
+const transmisson = "manual"
+const wheels = 4
